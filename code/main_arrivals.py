c0c16f777e54571601f8542c81d4b62a88318c75
eliransher <scherzter@gmail.com>
2025-06-12 12:56:54 +0300

    files moved

--- code/main_arrivals.py
+++ code/main_arrivals.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(r"C:\Users\Eshel\workspace\one.deep.moment"))
 sys.path.append(r'C:\Users\Eshel\workspace\butools2\Python')
+sys.path.append('../../one.deep.moment/')
 from utils_sample_ph import *
 import numpy as np
 import scipy.linalg
@@ -72,20 +73,84 @@ def get_PH_general_with_zeros(original_size):
 
     return a, T, torch.stack(ms)
 
+def compute_kl_mom_error(res_PH, a_orig, T_orig, mm):
+    kl_res = {}
+    for key in res_PH.keys():
+        curr_a = res_PH[key][0]
+        curr_T = res_PH[key][1]
+        with torch.no_grad():
+            kl_res[key] = kl_divergence(curr_a.cpu().numpy(), curr_T.cpu().numpy(), a_orig.cpu().numpy().flatten(),
+                                        T_orig.cpu().numpy())
+
+    df_res_mom_acc = pd.DataFrame([])
+
+    for key in res_PH.keys():
+        with torch.no_grad():
+            curr_a = res_PH[key][0]
+            curr_T = res_PH[key][1]
+            momss = compute_moments(curr_a.cpu(), curr_T.cpu(), curr_T.shape[0], 10)
+            curr_errors = torch.abs(100 * (torch.tensor(momss).flatten() - mm) / mm)
+            curr_errors = np.array(curr_errors)
+            curr_ind = df_res_mom_acc.shape[0]
+            df_res_mom_acc.loc[curr_ind, 'num_mom_fit'] = key
+            df_res_mom_acc.loc[curr_ind, 'kl_div'] = kl_res[key]
+            for mom in range(1, 1 + curr_errors.shape[0]):
+                df_res_mom_acc.loc[curr_ind, str(mom)] = curr_errors[mom - 1]
+
+    return df_res_mom_acc
+
 def ph_density(t, alpha, T):
     """Evaluate PH density f(t) = α e^{Tt} t_vec"""
     t_vec = -T @ np.ones((T.shape[0], 1))
     e_Tt = scipy.linalg.expm(T * t)
     return float(alpha @ e_Tt @ t_vec)
 
-def main():
+def main(orig_dist_type):
+    if sys.platform == 'linux':
+
+        dump_path = '/scratch/eliransc/mom_anal/fit_6_moms_examples'
+    else:
+        dump_path = r'C:\Users\Eshel\workspace\data\moment_anal'
+
 
     mod_num = np.random.randint(1,10000000)
+    if orig_dist_type == 'cox':
 
+        has_maximum = False
+        while not has_maximum:
 
-    has_maximum = False
-    while not has_maximum:
+            rand_val = np.random.rand()
+            if rand_val < 0.4:
+                ph_size = np.random.randint(6, 11)
+            elif rand_val < 0.95:
+                ph_size = np.random.randint(11, 80)
+            else:
+                ph_size = np.random.randint(80, 150)
 
+            a_orig, T_orig, moms, something = sample_coxian(ph_size, 10)
+            t_values = np.linspace(0.01, 3, 250)
+            f_values = np.array([ph_density(t, a_orig, T_orig) for t in t_values])
+
+            # Find local maxima
+            local_maxima_indices = argrelextrema(f_values, np.greater)[0]
+
+            # Check if at least one maximum
+            has_maximum = len(local_maxima_indices) > 0
+
+            mm = torch.tensor(np.array(compute_first_n_moments(a_orig, T_orig, 10)).ravel())
+
+            a_orig, T_orig = torch.tensor(a_orig), torch.tensor(T_orig)
+
+        # plt.plot(t_values, f_values, label="PH density")
+        # plt.plot(t_values[local_maxima_indices], f_values[local_maxima_indices], 'ro', label="Local maxima")
+        # plt.xlabel("t")
+        # plt.ylabel("f(t)")
+        # plt.legend()
+        # plt.title("Phase-Type Density")
+        # plt.grid()
+        # plt.show()
+
+    elif orig_dist_type == 'general':
         rand_val = np.random.rand()
         if rand_val < 0.4:
             ph_size = np.random.randint(6, 11)
@@ -94,31 +159,21 @@ def main():
         else:
             ph_size = np.random.randint(80, 150)
 
-        a_orig, T_orig, moms, something = sample_coxian(ph_size, 10)
-        t_values = np.linspace(0.01, 3, 250)
-        f_values = np.array([ph_density(t, a_orig, T_orig) for t in t_values])
+        a_orig, T_orig, mm = get_PH_general_with_zeros(ph_size)
+    else:
+        rand_val = np.random.rand()
+        if rand_val < 0.4:
+            ph_size = np.random.randint(6, 11)
+        elif rand_val < 0.9:
+            ph_size = np.random.randint(11, 80)
+        else:
+            ph_size = np.random.randint(80, 150)
 
-        # Find local maxima
-        local_maxima_indices = argrelextrema(f_values, np.greater)[0]
-
-        # Check if at least one maximum
-        has_maximum = len(local_maxima_indices) > 0
-
-    plt.plot(t_values, f_values, label="PH density")
-    plt.plot(t_values[local_maxima_indices], f_values[local_maxima_indices], 'ro', label="Local maxima")
-    plt.xlabel("t")
-    plt.ylabel("f(t)")
-    plt.legend()
-    plt.title("Phase-Type Density")
-    plt.grid()
-    plt.show()
-
-    # a_orig, T_orig, mm = get_PH_general_with_zeros(ph_size)
-    # a, T, _, _ = sample_coxian(ph_size, 10)
-    mm = torch.tensor (np.array(compute_first_n_moments(a_orig, T_orig, 10)).ravel())
-
-
-    a_orig, T_orig = torch.tensor(a_orig), torch.tensor(T_orig)
+        a_orig, T_orig, moms = sample(ph_size)
+        moms = np.array(moms).flatten()
+        mm = torch.tensor(moms)
+        a_orig = torch.tensor(a_orig)
+        T_orig = torch.tensor(T_orig)
 
 
 
@@ -155,9 +210,13 @@ def main():
     moments = mm[:6]  # torch.tensor([1.00000000e+00, 6.05028076e+00, 1.95626417e+02, 1.05513745e+04,  7.22395741e+05])
     # Service
     # moments = torch.tensor([1.00000000e+00, 2.55408739e+00, 9.81573904e+00, 4.75758155e+01, 2.73684422e+02])
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+    print('Original size of PH: ', ph_size)
+    print('Original PH type: ', orig_dist_type)
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 
     res_PH = {}
-    for num_moms in [2, 3, 4,5 ,6]:
+    for num_moms in [2, 3, 4, 5, 6]:
         print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
         print(num_moms)
         while True:
@@ -206,47 +265,37 @@ def main():
             block_sizes = create_block_sizes_new(ph_size)
 
             print('new ph size is: ', ph_size)
+        file_name = 'mod_num_' + str(mod_num) + '_num_diff_moms_' + str(len(list(res_PH.keys()))) + '_orig_type_' + orig_dist_type +'_origsize_' + str(ph_size) + '.pkl'
+
         try:
-            pkl.dump((res_PH, a_orig, T_orig, mm), open(os.path.join(r'C:\Users\Eshel\workspace\data\moment_anal','mod_num_'+str(mod_num)+'_num_diff_moms_'+str(len(list(res_PH.keys())))+'.pkl'), 'wb'))
+            df_res_mom_acc = compute_kl_mom_error(res_PH, a_orig, T_orig, mm)
+            pkl.dump((res_PH, a_orig, T_orig, mm, scv, skew, kurt, df_res_mom_acc), open(os.path.join(dump_path,file_name), 'wb'))
         except:
             print('res_PH not defined')
 
-
-
-    kl_res = {}
-    for key in res_PH.keys():
-        curr_a = res_PH[key][0]
-        curr_T = res_PH[key][1]
-        with torch.no_grad():
-            kl_res[key] = kl_divergence(curr_a.cpu().numpy(), curr_T.cpu().numpy(), a_orig.cpu().numpy(),
-                                        T_orig.cpu().numpy())
-
-    df_res_mom_acc = pd.DataFrame([])
-
-    for key in res_PH.keys():
-        with torch.no_grad():
-            curr_a = res_PH[key][0]
-            curr_T =res_PH[key][1]
-            momss = compute_moments(curr_a.cpu(), curr_T.cpu(), curr_T.shape[0], 10)
-            curr_errors = torch.abs(100 * (torch.tensor(momss).flatten() - mm) / mm)
-            curr_errors = np.array(curr_errors)
-            curr_ind = df_res_mom_acc.shape[0]
-            df_res_mom_acc.loc[curr_ind, 'num_mom_fit'] = key
-            df_res_mom_acc.loc[curr_ind, 'kl_div'] = kl_res[key]
-            for mom in range(1, 1 + curr_errors.shape[0]):
-                df_res_mom_acc.loc[curr_ind, str(mom)] = curr_errors[mom - 1]
-
-
     try:
-        pkl.dump((res_PH, a_orig, T_orig, mm, scv, skew, kurt, df_res_mom_acc), open(os.path.join(r'C:\Users\Eshel\workspace\data\moment_anal',
-                                                       'mod_num_' + str(mod_num) + '_num_diff_moms_' + str(
-                                                           len(list(res_PH.keys()))) + 'full.pkl'), 'wb'))
+        pkl.dump((res_PH, a_orig, T_orig, mm, scv, skew, kurt, df_res_mom_acc), open(os.path.join(dump_path,file_name), 'wb'))
     except:
         print('res_PH not defined')
 
 
 
-
 if __name__ == "__main__":
 
-    main()
+    for ind in range(5000):
+
+        my_list = ['cox' or 'erlang' or 'general']
+        item = random.choice(my_list)
+        orig_dist_type = item # 'cox' or 'erlang' or 'general'
+
+        main(orig_dist_type)
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
