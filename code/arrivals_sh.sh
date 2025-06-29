b698f550f9299cd34a39dc82731cdeeb9d7def96
eliransher <scherzter@gmail.com>
2025-06-29 19:53:20 +0300

    files moved

--- code/arrivals_sh.sh
+++ code/arrivals_sh.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 25000
 source /home/eliransc/projects/def-dkrass/eliransc/mom_match/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/mom_analysis/code/fit_moms_main.py
+python /home/eliransc/projects/def-dkrass/eliransc/mom_analysis/code/fit_moms_diff_matchers.py
