5a2d8b688614926ccb3e84445d9102cf4cab09e8
eliransher <scherzter@gmail.com>
2025-07-01 13:56:38 +0300

    files moved

--- code/matching_gpu.sh.sh
+++ code/matching_gpu.sh.sh
@@ -2,6 +2,6 @@
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
 #SBATCH --gpus-per-node=1
-#SBATCH --mem-per-cpu=30000M
+#SBATCH --mem-per-cpu=35000M
 source /home/eliransc/projects/def-dkrass/eliransc/mom_match/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/mom_analysis/code/fit_moms_diff_matchers.py
